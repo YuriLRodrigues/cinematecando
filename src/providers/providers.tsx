860a81c072f0c1eba3a19b48bef2cc59806c37f9
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-26 19:48:39 -0300

    feat: nextui provider include in all providers

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,11 +1,14 @@
 'use client'
+import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
 import React, { ReactNode } from 'react'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
-    </NextThemeProvider>
+    <NextUIProvider>
+      <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        {children}
+      </NextThemeProvider>
+    </NextUIProvider>
   )
 }
