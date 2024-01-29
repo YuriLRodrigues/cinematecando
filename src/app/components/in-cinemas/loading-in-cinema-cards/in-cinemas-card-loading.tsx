0e6f2c490df69d2205d0b67f3c29e92a3c515994
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-29 17:32:13 -0300

    feat: in cinemas card loading component

---
+++ src/app/components/in-cinemas/loading-in-cinema-cards/in-cinemas-card-loading.tsx
@@ -0,0 +1,5 @@
+export const InCinemasCardLoading = () => {
+  return (
+    <div className="min-h-96 min-w-56 animate-pulse cursor-pointer rounded-md bg-slate-300 p-2 dark:bg-slate-600 md:max-w-52" />
+  )
+}
