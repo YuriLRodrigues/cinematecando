949ba94e3ca71eb1871be11653ecce89804896fc
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-30 15:00:14 -0300

    feat: container variant

--- src/variants/container-variant.ts
+++ src/variants/container-variant.ts
@@ -3,12 +3,12 @@ import { tv } from 'tailwind-variants'
 export const ContainerVariants = tv({
   variants: {
     mt: {
-      4: 'md:mt-4 mt:2',
-      8: 'md:mt-8 mt:4',
-      10: 'md:mt-10 mt-5',
-      16: 'md:mt-16 mt-8',
-      20: 'md:mt-20 mt-10',
-      24: 'md:mt-24 mt-12',
+      4: 'md:mt-4 mt:3',
+      8: 'md:mt-8 mt:6',
+      10: 'md:mt-10 mt-7',
+      16: 'md:mt-16 mt-10',
+      20: 'md:mt-20 mt-12',
+      24: 'md:mt-24 mt-16',
     },
     width: {
       full: 'w-full',
