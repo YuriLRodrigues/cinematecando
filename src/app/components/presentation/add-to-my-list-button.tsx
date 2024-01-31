c670ad1bca4e9d0b36c36685129d23e9e6ee067b
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-31 19:42:57 -0300

    feat: add to my list button

--- src/app/components/presentation/add-to-my-list-button.tsx
+++ src/app/components/presentation/add-to-my-list-button.tsx
@@ -1,20 +1,35 @@
+'use client'
 import { Button } from '@nextui-org/react'
 import { ComponentProps } from 'react'
 
 import { cn } from '@/lib/utils'
-import { Plus } from 'lucide-react'
+import { useMovieStore } from '@/store/use-movie-store'
+import { Movie } from '@/types/movie'
+import { Plus, X } from 'lucide-react'
 
 type AddToMyListButtonProps = ComponentProps<'button'> & {
-  movieId: number
+  movie: Movie
 }
 
 export const AddToMyListButton = ({
   className,
-  movieId,
+  movie,
 }: AddToMyListButtonProps) => {
-  const addToList = (movieId: number) => {
-    console.log('adicionou o id: ', movieId) // arrumar e passar a lista/zustand
+  const { add, remove, verify } = useMovieStore((state) => ({
+    add: state.actions.addMovie,
+    remove: state.actions.removeMovie,
+    verify: state.actions.movieAlreadyExists,
+  }))
+
+  const handleAdd = () => {
+    add(movie)
   }
+
+  const handleRemove = () => {
+    remove(movie.id)
+  }
+
+  const isAdded = verify(movie.id)
 
   return (
     <Button
@@ -23,9 +38,10 @@ export const AddToMyListButton = ({
         'flex w-fit items-center justify-center gap-3 rounded-lg border border-white bg-transparent p-1 text-xs text-white md:p-2 md:text-base',
         className,
       )}
-      onClick={() => addToList(movieId)}
+      onClick={isAdded ? handleRemove : handleAdd}
     >
-      Minha lista <Plus />
+      {isAdded ? `Remover da lista` : `Minha lista`}
+      {isAdded ? <X className="text-red-500" /> : <Plus />}
     </Button>
   )
 }
