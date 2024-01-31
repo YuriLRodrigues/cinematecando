e107d4499dbbb1f1fb63186efb2d1f23e189998b
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-31 19:45:00 -0300

    feat: poster index

---
+++ src/app/movie/[slug]/components/poster/index.tsx
@@ -0,0 +1,15 @@
+import { PosterCard } from './poster-card'
+import { PosterDetailsSection } from './poster-details-section'
+import { PosterImage } from './poster-image'
+import { PosterImagesSlider } from './poster-images-slider'
+import { PosterRating } from './poster-rating'
+import { PosterRoot } from './poster-root'
+
+export const Poster = {
+  Root: PosterRoot,
+  Details: PosterDetailsSection,
+  Card: PosterCard,
+  Image: PosterImage,
+  Rating: PosterRating,
+  Images: PosterImagesSlider,
+}
