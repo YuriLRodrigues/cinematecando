7b4643d2128e55b1f3a0072be899d9f62de3611a
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:21:19 -0300

    feat: my list loading to suspense

---
+++ src/app/mylist/components/my-list-loading.tsx
@@ -0,0 +1,11 @@
+import { CardLoading } from '@/components/ui/card/card-loading'
+
+export const MyListLoading = () => {
+  return (
+    <div className="mx-auto my-4 grid w-full grid-cols-2 gap-x-4 gap-y-7 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      {Array.from({ length: 12 }).map((_, i) => (
+        <CardLoading key={i} />
+      ))}
+    </div>
+  )
+}
