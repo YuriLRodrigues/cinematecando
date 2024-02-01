1b5ff82de10d15e1a5b1a93b728feabeeed4f0bb
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:20:14 -0300

    feat: cast details page

---
+++ src/app/movie/[slug]/cast/page.tsx
@@ -0,0 +1,43 @@
+import { CastSliderByCrew } from './components/cast-slide-by-crew'
+import { Container } from '@/components/interface/container'
+
+import { movieFactory } from '@/infra/factory/movies.factory'
+
+type CastProps = {
+  params: { slug: number }
+}
+
+export default async function Cast({ params }: CastProps) {
+  const cast = await movieFactory().findCastersByMovieId({
+    id: params.slug,
+    token: process.env.API_BEARER_TOKEN!,
+  })
+
+  const findByCastDepartament = (departament: string) => {
+    return cast.crew.filter((cast) => cast.department === departament)
+  }
+
+  return (
+    <Container mt={24}>
+      <CastSliderByCrew
+        crew={findByCastDepartament('Directing')}
+        title="Diretor(a)"
+      />
+      <CastSliderByCrew
+        crew={findByCastDepartament('Writing')}
+        title="Escritor(a)"
+      />
+      <CastSliderByCrew
+        crew={findByCastDepartament('Production')}
+        title="Produtor(a)"
+      />
+      <CastSliderByCrew crew={findByCastDepartament('Art')} title="Arte" />
+      <CastSliderByCrew crew={findByCastDepartament('Sound')} title="Som" />
+      <CastSliderByCrew
+        crew={findByCastDepartament('Editing')}
+        title="Editor"
+      />
+      <CastSliderByCrew crew={findByCastDepartament('Camera')} title="Câmera" />
+    </Container>
+  )
+}
