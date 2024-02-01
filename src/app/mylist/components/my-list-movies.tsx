ee6102e257aed7ef45f714157c7b2e1fac69dbe6
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:21:29 -0300

    feat: my list movies component

--- src/app/mylist/components/my-list-movies.tsx
+++ src/app/mylist/components/my-list-movies.tsx
@@ -1,21 +1,24 @@
 'use client'
 
-import { useEffect } from 'react'
-
 import { Card } from '@/components/ui/card'
 
 import { useMovieStore } from '@/store/use-movie-store'
 
-export const MyListMovies = () => {
-  const { movies } = useMovieStore.getState()
+import { NoneItems } from './none-items'
 
-  return movies.map((movie) => (
-    <Card
-      key={movie.id}
-      movieIdToRemove={movie.id}
-      imageName={movie.title}
-      imageUrl={movie.poster_path}
-      movieId={movie.id}
-    />
-  ))
+export const MyListMovies = () => {
+  const movies = useMovieStore((state) => state.movies)
+
+  return movies.length > 0 ? (
+    <div className="mx-auto grid w-full grid-cols-2 gap-x-4 gap-y-7 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      {movies.map((movie) => (
+        <Card.Root key={movie.id}>
+          <Card.Image src={movie.poster_path} alt={`movie-${movie.title}`} />
+          <Card.Trigger.movie movie={movie} />
+        </Card.Root>
+      ))}
+    </div>
+  ) : (
+    <NoneItems />
+  )
 }
