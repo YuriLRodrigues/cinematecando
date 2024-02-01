d35f35df715e8cca83ade3c8808cedb0c595a808
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:19:46 -0300

    feat: added cast and recommendation components

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -1,7 +1,11 @@
 import { Suspense } from 'react'
 
+import { CastLoading } from './components/cast/cast-loading'
+import { CastWrapper } from './components/cast/cast-wrapper'
 import { PosterLoading } from './components/poster/poster-loading'
 import { PosterWrapper } from './components/poster/poster-wrapper'
+import { RecommendationsLoading } from './components/recommendations/recommendations-loading'
+import { RecommendationsWrapper } from './components/recommendations/recommendations-wrapper'
 import { Container } from '@/components/interface/container'
 
 // export async function generateStaticParams({
@@ -26,6 +30,12 @@ export default async function MovieDetails({ params }: MovieDetailsProps) {
     <Container mt={24}>
       <Suspense fallback={<PosterLoading />}>
         <PosterWrapper params={params.slug} />
+      </Suspense>
+      <Suspense fallback={<CastLoading />}>
+        <CastWrapper movieId={params.slug} />
+      </Suspense>
+      <Suspense fallback={<RecommendationsLoading />}>
+        <RecommendationsWrapper movieId={params.slug} />
       </Suspense>
     </Container>
   )
