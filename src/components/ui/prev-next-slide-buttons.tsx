9679af26b1827d55af246fb87ca1e2d2539c3415
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:21:38 -0300

    feat: buttons slide

--- src/components/ui/prev-next-slide-buttons.tsx
+++ src/components/ui/prev-next-slide-buttons.tsx
@@ -1,15 +1,29 @@
+import { ComponentProps } from 'react'
 import { BiLeftArrow, BiRightArrow } from 'react-icons/bi'
+
+import { cn } from '@/lib/utils'
 
 import { SlideButton } from '../ui/slide-button'
 
-type SlideButtonsProps = {
+type SlideButtonsProps = ComponentProps<'div'> & {
   prev: () => void
   next: () => void
 }
 
-export const PrevNextSlideButtons = ({ next, prev }: SlideButtonsProps) => {
+export const PrevNextSlideButtons = ({
+  next,
+  prev,
+  className,
+  ...props
+}: SlideButtonsProps) => {
   return (
-    <div className="absolute bottom-3 right-3 z-[30] flex w-fit items-center justify-center gap-3 duration-300">
+    <div
+      className={cn(
+        'absolute bottom-3 right-3 z-[30] flex w-fit items-center justify-center gap-3 duration-300',
+        className,
+      )}
+      {...props}
+    >
       <SlideButton onClick={prev}>
         <BiLeftArrow />
       </SlideButton>
