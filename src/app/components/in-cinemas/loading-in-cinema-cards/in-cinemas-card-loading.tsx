62115a7c6e9d82abfcf225c296cf9271e5b8cd70
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:16:54 -0300

    feat: default movie img

--- src/app/components/in-cinemas/loading-in-cinema-cards/in-cinemas-card-loading.tsx
+++ src/app/components/in-cinemas/loading-in-cinema-cards/in-cinemas-card-loading.tsx
@@ -1,5 +1,15 @@
+import { ImageBlurLoading } from '@/components/ui/image-blur-loading'
+
 export const InCinemasCardLoading = () => {
   return (
-    <div className="min-h-96 min-w-56 animate-pulse cursor-pointer rounded-md bg-slate-300 p-2 dark:bg-slate-600 md:max-w-52" />
+    <div className="min-h-96 min-w-56 animate-pulse cursor-pointer rounded-md bg-slate-300 p-2 dark:bg-slate-600 md:max-w-52">
+      <ImageBlurLoading
+        src={'/default-movie-card.png'}
+        width={500}
+        height={500}
+        className="aspect-square h-96 w-full rounded-md object-cover object-center duration-300 hover:scale-95"
+        alt={'loading-movie-card'}
+      />
+    </div>
   )
 }
