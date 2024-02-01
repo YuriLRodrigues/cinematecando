a7f4e923eb2dfb9e17144455891ea8f0f061a52a
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-01 19:21:03 -0300

    feat: my list page

--- src/app/mylist/page.tsx
+++ src/app/mylist/page.tsx
@@ -1,18 +1,26 @@
+import { Suspense } from 'react'
+
+import { MyListLoading } from './components/my-list-loading'
 import { MyListMovies } from './components/my-list-movies'
 import { Container } from '@/components/interface/container'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default function MyList() {
   return (
-    <Container mt={24} className="px-3 lg:px-16">
+    <Container
+      mt={24}
+      className="min-h-[calc(100vh-110px)] px-3 lg:min-h-[calc(100vh-210px)] lg:px-16"
+    >
       <Tabs defaultValue="movies" className="w-full flex-wrap">
         <TabsList className="bg-transparent">
           <TabsTrigger value="movies">Filmes</TabsTrigger>
           <TabsTrigger value="series">Séries</TabsTrigger>
         </TabsList>
-        <TabsContent value="movies">
-          <MyListMovies />
-        </TabsContent>
+        <Suspense fallback={<MyListLoading />}>
+          <TabsContent value="movies">
+            <MyListMovies />
+          </TabsContent>
+        </Suspense>
         <TabsContent value="series">Change your password here.</TabsContent>
       </Tabs>
     </Container>
