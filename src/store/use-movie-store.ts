e5b5c5c50b4d09726cbf00c2254aac397dc0ac6a
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 15:09:58 -0300

    feat: added actions of add all movies and series

--- src/store/use-movie-store.ts
+++ src/store/use-movie-store.ts
@@ -1,28 +1,44 @@
+'use client'
 import { Movie } from '@/types/movie'
+import { Serie } from '@/types/serie'
 import { create } from 'zustand'
 
 type ActionsProps = {
-  addMovie: (movie: Movie) => void
+  addAllMovies: (movies: Movie[]) => void
+  addAllSeries: (series: Serie[]) => void
+  addMovie: (movie: any) => void
   removeMovie: (movieId: number) => void
-  // addSerie: (serie: any) => void
-  // removeSerie: (serieId: number) => void
+  movieAlreadyExists: (movieId: number) => boolean
+  addSerie: (serie: any) => void
+  removeSerie: (serieId: number) => void
+  serieAlreadyExists: (serieId: number) => boolean
 }
 
 type MovieStoreProps = {
   movies: Movie[]
-  // series: MovieDetailsEntity[]
+  series: Serie[]
   actions: ActionsProps
 }
 
-export const useMovieStore = create<MovieStoreProps>((set) => ({
+export const useMovieStore = create<MovieStoreProps>((set, get) => ({
   movies: [],
   series: [],
 
   actions: {
+    addAllMovies: (movies: Movie[]) => {
+      return movies
+    },
+
     addMovie: (movie: any) => {
-      set((state) => ({
-        movies: [...state.movies, movie],
-      }))
+      set((state) => {
+        return {
+          movies: [...state.movies, movie],
+        }
+      })
+    },
+
+    movieAlreadyExists(movieId) {
+      return get().movies.some((movie) => movie.id === movieId)
     },
 
     removeMovie: (movieId: number) =>
@@ -30,15 +46,23 @@ export const useMovieStore = create<MovieStoreProps>((set) => ({
         movies: state.movies.filter((movie) => movie.id !== movieId),
       })),
 
-    // addSerie: (serie: any) => {
-    //   set((state) => ({
-    //     series: [...state.series, serie],
-    //   }))
-    // },
+    addAllSeries: (series: Serie[]) => {
+      return series
+    },
 
-    // removeSerie: (serieId: number) =>
-    //   set((state) => ({
-    //     movies: state.movies.filter((movie) => movie.id !== serieId),
-    //   })),
+    addSerie: (serie: any) => {
+      set((state) => ({
+        series: [...state.series, serie],
+      }))
+    },
+
+    removeSerie: (serieId: number) =>
+      set((state) => ({
+        series: state.series.filter((serie) => serie.id !== serieId),
+      })),
+
+    serieAlreadyExists(serieId) {
+      return get().series.some((serie) => serie.id === serieId)
+    },
   },
 }))
