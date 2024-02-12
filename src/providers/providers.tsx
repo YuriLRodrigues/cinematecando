f9e81ad071be13a097bdf9ebae88813ac88ac77a
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 15:09:41 -0300

    feat: added local storage get and save items in useMovieStore

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,9 +1,22 @@
 'use client'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
+
+import { useMovieStore } from '@/store/use-movie-store'
+import { Movie } from '@/types/movie'
+import { Serie } from '@/types/serie'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
+  const [movies, setMovies] = useLocalStorage<Movie[]>('movies', [])
+  const [series, setSeries] = useLocalStorage<Serie[]>('series', [])
+
+  useEffect(() => {
+    useMovieStore.setState({ movies: movies })
+    useMovieStore.setState({ series: series })
+  }, [movies, series])
+
   return (
     <NextUIProvider>
       <NextThemeProvider attribute="class" defaultTheme="dark" enableSystem>
