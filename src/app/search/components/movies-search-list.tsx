7433f84d1138ac037002ec3ddfb3ee23a5bbf59b
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 13:43:10 -0300

    feat: movies and series list and loading

---
+++ src/app/search/components/movies-search-list.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+
+import { Card } from '@/components/ui/card'
+import { PaginationApp } from '@/components/ui/pagination-app'
+
+import { searchFactory } from '@/infra/factory/search.factory'
+
+import { NoneItems } from './none-items'
+
+type MoviesSearchListProps = {
+  page?: string
+  search: string
+}
+
+export const MoviesSearchList = async ({
+  page,
+  search,
+}: MoviesSearchListProps) => {
+  const moviesFound = await searchFactory().findAllMovies({
+    page: page ?? '1',
+    search,
+    token: process.env.API_BEARER_TOKEN!,
+  })
+  console.log(moviesFound)
+
+  return moviesFound.results.length > 0 ? (
+    <>
+      <div className="mx-auto my-6 grid w-full grid-cols-2 gap-x-4 gap-y-7 sm:grid-cols-3 md:grid-cols-4 lg:my-12 lg:grid-cols-5 xl:grid-cols-6">
+        {moviesFound.results.map((movie) => (
+          <Card.Root key={movie.id}>
+            <Link href={`movie/${movie.id}`}>
+              <Card.Image
+                src={movie.poster_path}
+                alt={`movie-${movie.title}`}
+              />
+            </Link>
+            <Card.Trigger.movie movie={movie} />
+          </Card.Root>
+        ))}
+      </div>
+
+      <PaginationApp
+        actual_page={page ?? '1'}
+        link="/search"
+        total_pages={moviesFound.total_pages}
+      />
+    </>
+  ) : (
+    <NoneItems title="Nenhum filme encontrado" />
+  )
+}
---
+++ src/app/search/components/series-search-list.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+
+import { Card } from '@/components/ui/card'
+import { PaginationApp } from '@/components/ui/pagination-app'
+
+import { searchFactory } from '@/infra/factory/search.factory'
+
+import { NoneItems } from './none-items'
+
+type SeriesSearchListProps = {
+  page?: string
+  search: string
+}
+
+export const SeriesSearchList = async ({
+  page,
+  search,
+}: SeriesSearchListProps) => {
+  const seriesFound = await searchFactory().findAllSeries({
+    page: page ?? '1',
+    search,
+    token: process.env.API_BEARER_TOKEN!,
+  })
+
+  console.log(seriesFound)
+
+  return seriesFound.results.length > 0 ? (
+    <>
+      <div className="mx-auto my-6 grid w-full grid-cols-2 gap-x-4 gap-y-7 sm:grid-cols-3 md:grid-cols-4 lg:my-12 lg:grid-cols-5 xl:grid-cols-6">
+        {seriesFound.results.map((serie) => (
+          <Card.Root key={serie.id}>
+            <Link href={`serie/${serie.id}`}>
+              <Card.Image src={serie.poster_path} alt={`serie-${serie.name}`} />
+            </Link>
+            <Card.Trigger.serie serie={serie} />
+          </Card.Root>
+        ))}
+      </div>
+
+      <PaginationApp
+        actual_page={page ?? '1'}
+        link="/search"
+        total_pages={seriesFound.total_pages}
+      />
+    </>
+  ) : (
+    <NoneItems title="Nenhum filme encontrado" />
+  )
+}
