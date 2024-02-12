5f918826bef6330ebc420baf60a13bae9bb41573
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 13:48:11 -0300

    feat: list series loading component

---
+++ src/app/series/components/list-series-loading.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+import { Card } from '@/components/ui/card'
+import { ContainerCard } from '@/components/ui/card/container-card'
+
+export const ListSeriesLoading = () => {
+  return (
+    <ContainerCard className="my-10 px-3 lg:px-16">
+      {Array.from({ length: 10 }).map((_, item) => (
+        <Card.Root key={item} className="animate-pulse">
+          <Image
+            src={'/default-movie-card.png'}
+            alt="movie-card-loading"
+            width={500}
+            height={500}
+          />
+        </Card.Root>
+      ))}
+    </ContainerCard>
+  )
+}
