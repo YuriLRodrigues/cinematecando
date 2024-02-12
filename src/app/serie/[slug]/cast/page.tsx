0ff5e70058ad57a4f9d1eab465d894112622a191
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 13:44:00 -0300

    feat: all cast page

---
+++ src/app/serie/[slug]/cast/page.tsx
@@ -0,0 +1,43 @@
+import { CastSliderByCrew } from './components/cast-slide-by-crew'
+import { Container } from '@/components/interface/container'
+
+import { serieFactory } from '@/infra/factory/series.factory'
+
+type CastProps = {
+  params: { slug: number }
+}
+
+export default async function Cast({ params }: CastProps) {
+  const cast = await serieFactory().findCreditsBySerieId({
+    id: params.slug,
+    token: process.env.API_BEARER_TOKEN!,
+  })
+
+  const findByCastDepartament = (departament: string) => {
+    return cast.crew.filter((cast) => cast.department === departament)
+  }
+
+  return (
+    <Container mt={24} className="px-3 lg:px-16">
+      <CastSliderByCrew
+        crew={findByCastDepartament('Directing')}
+        title="Diretor(a)"
+      />
+      <CastSliderByCrew
+        crew={findByCastDepartament('Writing')}
+        title="Escritor(a)"
+      />
+      <CastSliderByCrew
+        crew={findByCastDepartament('Production')}
+        title="Produtor(a)"
+      />
+      <CastSliderByCrew crew={findByCastDepartament('Art')} title="Arte" />
+      <CastSliderByCrew crew={findByCastDepartament('Sound')} title="Som" />
+      <CastSliderByCrew
+        crew={findByCastDepartament('Editing')}
+        title="Editor"
+      />
+      <CastSliderByCrew crew={findByCastDepartament('Camera')} title="Câmera" />
+    </Container>
+  )
+}
