a89178e90709e4f0cbcc18c80e21850dee1af948
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 13:23:17 -0300

    feat: serie types

---
+++ src/types/serie.ts
@@ -0,0 +1,17 @@
+export type Serie = {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: Array<number>
+  id: number
+  origin_country: string
+  original_name: string
+  original_language: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  first_air_date: string
+  name: string
+  vote_average: number
+  vote_count: number
+}
