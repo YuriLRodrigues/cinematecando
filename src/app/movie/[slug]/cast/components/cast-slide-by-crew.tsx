9c4278e1652e26f57280956e33e16c36e5b0ac3d
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-12 13:40:22 -0300

    feat: validation length

--- src/app/movie/[slug]/cast/components/cast-slide-by-crew.tsx
+++ src/app/movie/[slug]/cast/components/cast-slide-by-crew.tsx
@@ -32,71 +32,73 @@ type CastSliderByCrewProps = {
 
 export const CastSliderByCrew = ({ crew, title }: CastSliderByCrewProps) => {
   return (
-    <>
-      <Swiper
-        rewind={true}
-        modules={[FreeMode, Parallax]}
-        parallax={true}
-        className="mySwiper relative !mb-10 !mt-10 max-w-full !px-2 !pt-14 md:!mt-20 lg:!ml-16 lg:!px-0"
-        slidesPerView={'auto'}
-        breakpoints={{
-          10: {
-            spaceBetween: 15,
-          },
-          768: {
-            spaceBetween: 20,
-          },
-          1024: {
-            spaceBetween: 30,
-          },
-          1440: {
-            spaceBetween: 40,
-          },
-        }}
-      >
-        <Heading
-          HeadingType="h2"
-          size="sm"
-          className="absolute left-0 top-0 z-[30] pl-3 md:pl-0"
+    crew.length > 0 && (
+      <>
+        <Swiper
+          rewind={true}
+          modules={[FreeMode, Parallax]}
+          parallax={true}
+          className="mySwiper relative !mb-10 !mt-10 max-w-full !px-2 !pt-14 md:!mt-20 lg:!ml-16 lg:!px-0"
+          slidesPerView={'auto'}
+          breakpoints={{
+            10: {
+              spaceBetween: 15,
+            },
+            768: {
+              spaceBetween: 20,
+            },
+            1024: {
+              spaceBetween: 30,
+            },
+            1440: {
+              spaceBetween: 40,
+            },
+          }}
         >
-          {title}
-        </Heading>
-
-        {crew.map((people) => (
-          <SwiperSlide
-            key={people.id}
-            className="min-h-96 min-w-52 max-w-56 cursor-pointer rounded-md"
+          <Heading
+            HeadingType="h2"
+            size="sm"
+            className="absolute left-0 top-0 z-[30] pl-3 md:pl-0"
           >
-            <FlexContainer
-              variant="centered-col"
-              className="absolute bottom-0 z-[20] flex h-full w-full flex-col justify-end bg-gradient-to-t from-dark-blue-main/70 to-transparent px-2"
+            {title}
+          </Heading>
+
+          {crew.map((people) => (
+            <SwiperSlide
+              key={people.id}
+              className="min-h-96 min-w-52 max-w-56 cursor-pointer rounded-md"
             >
-              <Heading
-                HeadingType="h3"
-                position="centered"
-                size="xs"
-                className="line-clamp-1 text-white"
+              <FlexContainer
+                variant="centered-col"
+                className="absolute bottom-0 z-[20] flex h-full w-full flex-col justify-end bg-gradient-to-t from-dark-blue-main/70 to-transparent px-2"
               >
-                {people.name}
-              </Heading>
-              <Text
-                variant="subtitle"
-                position="centered"
-                className="line-clamp-1"
-              >
-                {people.known_for_department}
-              </Text>
-            </FlexContainer>
-            <ImageBlurLoading
-              src={`${people.profile_path ? `https://image.tmdb.org/t/p/original${people.profile_path}` : '/default-img.png'}`}
-              width={500}
-              height={500}
-              className="aspect-square h-96 w-full rounded-md object-cover object-center duration-300 hover:scale-95"
-              alt={people.name}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+                <Heading
+                  HeadingType="h3"
+                  position="centered"
+                  size="xs"
+                  className="line-clamp-1 text-white"
+                >
+                  {people.name}
+                </Heading>
+                <Text
+                  variant="subtitle"
+                  position="centered"
+                  className="line-clamp-1"
+                >
+                  {people.known_for_department}
+                </Text>
+              </FlexContainer>
+              <ImageBlurLoading
+                src={`${people.profile_path ? `https://image.tmdb.org/t/p/original${people.profile_path}` : '/default-img.png'}`}
+                width={500}
+                height={500}
+                className="aspect-square h-96 w-full rounded-md object-cover object-center duration-300 hover:scale-95"
+                alt={people.name}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
   )
 }
